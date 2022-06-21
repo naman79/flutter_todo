9a31b393270e074b2d4cee5cfdd3861022e46158
kangminsu79 <minsu.kang@timeblocks.com>
2022-06-22 06:46:42 +0900

    add todo list and event

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_todo/screens/list_screen.dart';
+import 'package:flutter_todo/screens/login_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Future<bool> checkLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isLogin = prefs.getBool('isLogin') ?? false;
+    print("[*] isLogin" + isLogin.toString());
+    return isLogin;
+  }
+
+  void moveScreen() async {
+    await checkLogin().then((isLogin) {
+      if (isLogin) {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => ListScreen()));
+      } else {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => LoginScreen()));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      moveScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'SplashScreen',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '나만의 일정 관리 : TODO 리스트 앱',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
